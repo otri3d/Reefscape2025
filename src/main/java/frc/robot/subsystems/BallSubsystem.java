34d88e88b85ffe9fa2b40566a2a6893051a5a3b1
GarfiedtheDestroyer <dave.paquette@ontariotechu.net>
2025-01-05 11:46:32 -0500

    removed clutter

--- src/main/java/frc/robot/subsystems/BallSubsystem.java
+++ src/main/java/frc/robot/subsystems/BallSubsystem.java
@@ -14,11 +14,13 @@ public class BallSubsystem extends SubsystemBase {
   private double speedON = 1.0;
   private double clawConstant = 0.4;
 
-  /** Creates a new ExampleSubsystem. */
+  /** Creates a new Ball. */
 
   public BallSubsystem() {
+
     ball1 = new WPI_VictorSPX(OperatorConstants.MOTORCONTROLPORT7);
     ball2 = new WPI_VictorSPX(OperatorConstants.MOTORCONTROLPORT8);
+
     claw1 = new WPI_VictorSPX(OperatorConstants.MOTORCONTROLPORT9);
     claw2 = new WPI_VictorSPX(OperatorConstants.MOTORCONTROLPORT10);
   }
@@ -46,25 +48,5 @@ public class BallSubsystem extends SubsystemBase {
       intake();
     ball1.setInverted(!ball1.getInverted());
     ball2.setInverted(!ball2.getInverted());
-  }
-
-  /**
-   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
-  public boolean exampleCondition() {
-    // Query some boolean state, such as a digital sensor.
-    return false;
-  }
-
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
-
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
   }
 }
