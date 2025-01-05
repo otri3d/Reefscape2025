9d95d65fa197c05a205284b7422d4a8681d9004a
Kershan Arulneswaran <74938655+Koops0@users.noreply.github.com>
2025-01-05 10:48:21 -0500

    finished ArmSubsystem code(?), added Coral and Algae commands

--- src/main/java/frc/robot/commands/CoralCommand.java
+++ src/main/java/frc/robot/commands/CoralCommand.java
@@ -2,7 +2,6 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 
-public class DefaultArmCommand extends Command{
-
+public class CoralCommand extends Command{
     
 }
--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -4,6 +4,7 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.Solenoid;
+//import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 
@@ -16,18 +17,36 @@ import frc.robot.Constants.DriveConstants;
 /* Subsystem for the coral arm*/
 public class ArmSubsystem extends SubsystemBase{
 
+    // Variables
     private WPI_VictorSPX m_armMotor1;
     private WPI_VictorSPX m_armMotor2;
     private Solenoid m_solenoid;
 
     //private DoubleSolenoid m_solenoid;
 
+    //Public constructor
     public ArmSubsystem(){
         m_armMotor1 = new WPI_VictorSPX(OperatorConstants.MOTORCONTROLPORT5);
         m_armMotor2 = new WPI_VictorSPX(OperatorConstants.MOTORCONTROLPORT6);
 
         m_solenoid = new Solenoid(PneumaticsModuleType.CTREPCM, OperatorConstants.SOLENOID);
         //m_solenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1);
+    }
+
+    // Change Solenoid values to open/close arm 
+    public void liftArm(){
+        m_solenoid.set(true);
+    }
+
+    // Move the arm up and down
+    public void moveArm(double speed){
+        m_armMotor1.set(speed);
+        m_armMotor2.set(speed);
+    }
+
+    //Check if system is open
+    public boolean isOpen(){
+        return m_solenoid.get();
     }
     
   /**
