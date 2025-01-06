1396cfbb7b9ba7a7837c9b11db8dc69453cac7cf
GarfiedtheDestroyer <dave.paquette@ontariotechu.net>
2025-01-06 00:26:18 -0500

    renaming ballsubsystem

--- src/main/java/frc/robot/subsystems/BallSubsystem.java
+++ src/main/java/frc/robot/subsystems/BallSubsystem.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -10,22 +6,24 @@ import frc.robot.Constants.OperatorConstants;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 public class BallSubsystem extends SubsystemBase {
-  private WPI_VictorSPX claw1;
+  private WPI_VictorSPX claw;
   private double speedON = 1.0;
 
   /** Creates a new BallSubsystem. */
 
   public BallSubsystem() {
-    claw1 = new WPI_VictorSPX(OperatorConstants.MOTORCONTROLPORT7);
+    claw = new WPI_VictorSPX(OperatorConstants.MOTORCONTROLPORT7);
   }
 
+  //sets the speed of the motor to the max
   public void claw() {
-    claw1.set(speedON);
+    claw.set(speedON);
   }
 
+  //switches the direction of the motor, CW -> CCW or CCW -> CW
   public void invertClaw() {
-    if(claw1.get() == 0.0)
+    if(claw.get() == 0.0)
       claw();
-    claw1.setInverted(!claw1.getInverted());
+    claw.setInverted(!claw.getInverted());
   }
 }
